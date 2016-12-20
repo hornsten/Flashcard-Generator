50f26f4794332081d5117c12cd7714f2b5900f01
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-12-20 17:13:53 -0600

    commit

--- flashcard.js
+++ flashcard.js
@@ -1,8 +1,8 @@
 var SimpleCard = require("./Simple");
 var Cloze = require("./Cloze");
 
-var card3 = new SimpleCard('How\'s it going?', 'Not so great.');
-var card5 = new Cloze('Clock', 'A [cloze] will tell you what time it is');
+var card3 = new SimpleCard('In Greek mythology, who was the queen of the underworld and wife of Hades?', 'Persephone');
+var card5 = new Cloze('black', 'According to legend, a unicorn\'s horn is white at the base, [cloze] in the middle and red at the tip');
 
 card3.printCard();
 
